213f5153c8ffd74d2acf4c25236007af50928ad2
itulahore <itulahore56@gmail.com>
2024-02-07 12:13:55 +0500

    Simple style changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
   return (
     <div className="bg-gray-200">
       <div className="mx-2 lg:mx-10 flex flex-col">
-        <h3 className=" text-4xl my-3 font-mono">Our Heros</h3>
+        <h3 className=" text-4xl my-5 flex justify-center">Our Heros</h3>
         <div className="flex justify-center flex-wrap ">
           {data?.map(({ id, name, image }) => (
             <Card key={id} id={id} name={name} image={image} />
